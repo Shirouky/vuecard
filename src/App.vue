e95213c3b4aa143c57c6aef966c891ca5ed95f32
Luba Derebas <luba_d2005@mail.ru>
2021-12-10 19:55:51 +0300

    User card with base name

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <UserCard
+      :username="username"
+      :name="name"
+      :surname="surname"
+      :location="location"
+      :mail="mail"
+      :phone="phone"
+    />
+  </div>
+</template>
+
+<script>
+import UserCard from "./components/UserCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      username: "romashka",
+      name: "Иван",
+      surname: "Иванов",
+      location: "Москва, Юбилейная 50",
+      mail: "coldrabbit48@example.com",
+      phone: "+7-495-266-57-34",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
